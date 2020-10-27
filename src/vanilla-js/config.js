e05d9b997f77f2f0f06d20109845d9c1316362fc
Jennahh <41105078+Jennahh@users.noreply.github.com>
2020-10-27 12:26:03 -0400

    src_updated

--- src/vanilla-js/config.js
+++ src/vanilla-js/config.js
@@ -13,7 +13,7 @@ var config = {
           id: 'slug-style-id-1',
           title: 'Chicago',
           // image: './path/to/image/source.png',
-          description: 'Chicago.',
+          description: 'Welcome to Chicago. The city is home to 8.865 million inhabitants. The city is also much more diverse compared to other major US cities. Over 30% of the population is Black. The median income is roughly $57,000 with large racial disparities. THe median income for a white household is around $71,000 compared to $30,000 and $41,000 for Black and Latinx households, respectively. Chicago is not immune homelessness either. Chicago has over 86,000 homeless folks, 78% of whom are Black.',
           location: {
               center: [-87.62584, 41.87269],
               zoom: 9,
@@ -37,7 +37,7 @@ var config = {
             id: 'slug-style-id',
             title: 'Affordable Rental Housing Developments',
             // image: './path/to/image/source.png',
-            description: 'Affordable Rental Housing Developments in Chicago.',
+            description: 'we would like to create an interactive data visualization that puts forth a policy case for how vacant city-owned parcels can be leveraged as permanently affordable housing and incorporation into a community land trust in another city, namely Chicago. We propose to focus on Chicago because of good data availability through the city’s open data portal. Our opening visualization will be a mapbox of affordable housing in Chicago.',
             location: {
                 center: [-87.62584, 41.87269],
                 zoom: 11,
@@ -61,7 +61,7 @@ var config = {
             id: 'other-identifier',
             title: 'City-Owned Land Inventory',
             // image: './path/to/image/source.png',
-            description: 'City-Owned Land Inventory in Chicago',
+            description: 'This map will contrast the affordable rental housing developments. There is much more city owned land inventory than affordable housing, indicating some could be converted or put into a community land trust.',
             location: {
                 center: [-87.62584, 41.87269],
                 zoom: 11,
